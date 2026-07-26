2752e63921f7e4b063dd2a73a214973f5cff8ebf
Sargun Vohra <sargunv@users.noreply.github.com>
2026-07-25 22:39:29 -0700

    fix: let an ignored region own the newline that ends its last line comment (#66)

---
+++ fixtures/java/style/declarations/formatter-ignore-trailing-line-comment.java
@@ -0,0 +1,6 @@
+class FormatterIgnoreTrailingLineComment {
+  // @formatter:off
+  int raw   =1+2; // trailing the last ignored member
+  // @formatter:on
+  int after = 2;
+}
---
+++ fixtures/kotlin/style/declarations/formatter-ignore-trailing-line-comment.kt
@@ -0,0 +1,6 @@
+class FormatterIgnoreTrailingLineComment {
+  // @formatter:off
+  val raw   =1+2 // trailing the last ignored member
+  // @formatter:on
+  val after = 2
+}
